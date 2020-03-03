f0921ec88df312ac0335585e87dbb1dca1332ddf
Blake <blake@futchi.com>
2020-03-03 16:29:27 +0100

    setup route to authorize user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'api/auth', skip: [:omniauth_callbacks]
   namespace :api do
     resources :products, only: [:index]
     namespace :v0 do
