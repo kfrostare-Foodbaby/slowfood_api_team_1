e044047efe92104f68f0b489fbe86fac16491fc7
emtalen <59577366+emtalen@users.noreply.github.com>
2020-03-09 20:00:28 +0100

    modifying the test for new_order to pass by taking out the confirm order_id false

--- spec/requests/api/new_order_spec.rb
+++ spec/requests/api/new_order_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Api::OrdersController, type: :request do
     post '/api/orders', params: { product_id: product_1.id }
     order_id = (response_json)['order']['id']
     @order = Order.find(order_id)
-    order_id.confirm = false
   end
   
   describe 'POST /api/orders' do
